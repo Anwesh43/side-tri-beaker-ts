60b03d1f8cbc2467d51f61084668d1ed66a572fa
Anwesh43 <anweshthecool0@gmail.com>
2020-12-03 22:18:08 +0530

    adding STBNode class

--- index.ts
+++ index.ts
@@ -140,3 +140,45 @@ class Animator {
         }
     }
 }
+
+class STBNode {
+    
+    prev : STBNode
+    next : STBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new STBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSTBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : STBNode {
+        var curr : STBNode = this.prev
+        if (dir == 1) {
+            curr = this.next 
+        } 
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
